57d59fa892897345b07b90d40b11a8dd0f942a68
Brice Bartoletti <bartolettibrice@gmail.com>
2019-11-23 23:56:44 +0100

    jasmine 2 done

--- 3.1.Test2/questions.js
+++ 3.1.Test2/questions.js
@@ -2,37 +2,43 @@ let CreationTableauLangages =  () => ["Html", "CSS", "Java", "PHP"]
 
 let CreationTableauNombres =  () => [...Array(6).keys()]
 
+// Les 2 méthodes fonctionnent
 // let RemplacementElement =  (langages) => langages.map((lg,i) => i === 2 ? "Javascript" : lg)
-let RemplacementElement =  (langages) => langages.splice(2, 1, 'Javascript')
+let RemplacementElement =  (langages) => {
+    langages.splice(2, 1, 'Javascript')
+    return langages
+}
 
+// Les 2 méthodes fonctionnent
+// let AjoutElementLangages =  (langages) => {
+//     langages.push('Ruby')
+//     langages.push('Python')
+//     return langages
+// }
 let AjoutElementLangages =  (langages) => {
-
+    langages.splice(4, 0, 'Ruby', 'Python')
+    return langages
 }
 
 let AjoutElementNombres =  (nombres) => {
-
+    nombres.splice(0, 0, -2, -1)
+    return nombres
 }
 
 let SuppressionPremierElement =  (langages) => {
-
+    langages.shift()  // marche aussi avec splice(0,1)
+    return langages
 }
 
 let SuppressionDernierElement =  (langages) => {
-
+    langages.pop()
+    return langages
 }
 
-let ConversionChaineTableau =  (reseaux_sociaux_chaine) => {
+let ConversionChaineTableau =  (reseaux_sociaux_chaine) => reseaux_sociaux_chaine.split(",")
 
-}
+let ConversionTableauChaine =  (langages) => langages.join(",")
 
-let ConversionTableauChaine =  (langages) => {
+let TriTableau =  (reseaux_sociaux) => reseaux_sociaux.sort()
 
-}
-
-let TriTableau =  (reseaux_sociaux) => {
-
-}
-
-let InversionTableau =  (reseaux_sociaux) =>{
-
-}
+let InversionTableau =  (reseaux_sociaux) =>reseaux_sociaux.reverse()
